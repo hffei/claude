fc6aa8125145b44012004e3405a1dd891ca35a54
hffei <hffei_cn@hotmail.com>
2023-11-20 16:03:01 +0800

    修正中文文件名问题

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def chat():
     # 检查并保存上传的文件
     file = request.files.get('file')
     if file and file.filename != '':
-        filename = secure_filename(file.filename)
+        filename = file.filename
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
         files = {'file': (filename, open(file_path, 'rb'))}
@@ -41,6 +41,7 @@ def chat():
     
     response = resp.json()
     return response
+
 
 # 启动应用
 if __name__ == '__main__':
